ba6e971b58d2cbd307bfa0898e295df7bf78b3f9
David A <david.allert@hotmail.com>
2024-09-15 17:22:57 +0200

    Added some json routes, added the Mongodb Atlas dsn and edited package.json.

--- db/database.js
+++ db/database.js
@@ -1,11 +1,15 @@
+// sudo service mongod start
+require('dotenv').config()
+
 const mongo = require("mongodb").MongoClient;
 // const config = require("./config.json");
-const collectionName = "crowd";
+const collectionName = "documents";
 const morgan = require('morgan');
 
 const database = {
     getDb: async function getDb () {
-        let dsn = `mongodb://localhost:27017/mumin`;
+        // let dsn = `mongodb://localhost:27017/docs`;
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster.fatuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
