982af7255d2c1b85b456fce18a012523ab28b6e2
David A <david.allert@hotmail.com>
2024-09-17 21:02:44 +0200

    Changed db PW.

--- db/database.js
+++ db/database.js
@@ -10,6 +10,7 @@ const database = {
     getDb: async function getDb () {
         // let dsn = `mongodb://localhost:27017/docs`;
         let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster.fatuf.mongodb.net/docs?retryWrites=true&w=majority&appName=Cluster`
+        
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
