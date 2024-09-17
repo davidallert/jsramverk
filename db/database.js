761aa633a8d56e81291a088dffd706654768b086
David A <david.allert@hotmail.com>
2024-09-17 21:58:19 +0200

    Changed db client options.

--- db/database.js
+++ db/database.js
@@ -10,7 +10,6 @@ const database = {
     getDb: async function getDb () {
         // let dsn = `mongodb://localhost:27017/docs`;
         let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster.fatuf.mongodb.net/docs?retryWrites=true&w=majority&appName=Cluster`
-        
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
@@ -18,7 +17,9 @@ const database = {
             app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
         }
 
-        const client  = await mongo.connect(dsn);
+        const client  = await mongo.connect(dsn, {
+            useUnifiedTopology: true
+        });
         const db = await client.db();
         const collection = await db.collection(collectionName);
 
