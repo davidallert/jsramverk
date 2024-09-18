5b5bf0179b09381a717e93c66bdc6ef677f447c1
David A <david.allert@hotmail.com>
2024-09-18 11:12:37 +0200

    Added global Azure vars.

--- db/database.js
+++ db/database.js
@@ -1,5 +1,7 @@
 // sudo service mongod start
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
 
 const mongo = require("mongodb").MongoClient;
 // const config = require("./config.json");
